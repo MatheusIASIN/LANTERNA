af166e90eb2b2fd6fb3f47654e115adc31bba760
Matheus Iasin <55819881+MatheusIASIN@users.noreply.github.com>
2024-03-04 21:28:11 -0300

    Update pontozero.py

--- pontozero.py
+++ pontozero.py
@@ -1,1 +1,7 @@
+#isso é só um teste para ve se o github responde ao pycharm estou testando outros editores para ve a consistencia  e a integridafdde do código em outras
+plataformas isso por  hora vai ser executado no vs code pycharm e o jupyter notebook  claro que irei testa outros notebooks mais isso pode limitarcertas coisas 
+estou ciente dessas anomalias sei que as bibliotecas podem ser mais tangiveis e cheio de falhas online uma vez
+que a bibliotecas devem ser instaladas na maquina em questãso pous usa os compliadores e as ide notebooks onlline estam um pouco limitadas mais preciso ir contra o tempo para ter mais produtividades
+relatorio!
 print("Olá Mundo!")
+print(" Me livrei da maldição!")
